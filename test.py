9bae2535bcbb6371f6941fb7c78e1833a4916f36
NAVEEN KUMAR A <69150997+naveenkumar-ct19@users.noreply.github.com>
2026-01-30 16:46:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 friends = ['john', 'pat', 'gary', 'michael']
-for i, name in enumerate(friends):
-    print (f"iteration {i} is {name}")
+
+print(f"{'Iteration':<10} | {'Name':<10}")
+print("-" * 25)
+
+for i, name in enumerate(friends, start=1):
+    print(f"{i:<10} | {name.capitalize():<10}")
